60a1d636c7adf9403331dcce533b7ff105543455
Punkinut <heypunkinut@gmail.com>
2021-07-28 11:58:30 +1000

    Deleted GlobalState comment

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -5,24 +5,3 @@ import store from './store';
 export default function StoreProvider (props) {
   return <Provider store={store} {...props} />;
 }
-
-// const StoreContext = createContext();
-// const { Provider } = StoreContext;
-
-// const StoreProvider = ({ value = [], ...props }) => {
-//   const [state, dispatch] = useProductReducer({
-//     products: [],
-//     cart: [],
-//     cartOpen: false,
-//     categories: [],
-//     currentCategory: '',
-//   });
-
-//   return <Provider value={[state, dispatch]} {...props} />;
-// };
-
-// const useStoreContext = () => {
-//   return useContext(StoreContext);
-// };
-
-// export { StoreProvider, useStoreContext };
