81459a2c2d72771132f75b8ba78913a6d54a62e6
Punkinut <heypunkinut@gmail.com>
2021-07-28 11:04:40 +1000

    Setup basic files

---
+++ client/build/precache-manifest.42a6f0b08ef4556e8df6b7cfc776b9c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368f00faaa830f01450f816ba4439c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85acddc92f3900ca234f",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "7a49f11edb1673c28887",
+    "url": "/static/js/2.0e0df75e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.0e0df75e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85acddc92f3900ca234f",
+    "url": "/static/js/main.53927ee2.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
